7ef88323a003be2ff79efc4feca2a01e021e14a8
jorchevega <jorge.vega@evo-code.com>
2016-10-12 14:45:06 -0700

    Trabajo con matrices

--- JavaFondo/src/practicas/MatrizSimetrica.java
+++ JavaFondo/src/practicas/MatrizSimetrica.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practicas;
 import java.util.Scanner;
+
 /**
- *
- * @author jorge
+ * Practica 26 - Crea una matriz de 3x3 llena de datos la matriz y comprueba si 
+ * la matriz es simatrica o no
+ * @author escribe tu nombre aqui
  */
+
 public class MatrizSimetrica {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
@@ -16,26 +14,21 @@ public class MatrizSimetrica {
         int t[][];
         boolean simetrica;
         int i,j;
-        t = new int[4][4];
+        t = new int[3][3];
         
-        for (i=0;i<4;i++){
-            for (j=0;j<4;j++){
+        for (i=0;i<3;i++){
+            for (j=0;j<3;j++){
                 System.out.print("Introduzca elemento ["+i+"]["+j+"]: ");
                 t[i][j] = leer.nextInt();
             }
         }
-        simetrica=true; // suponemos que la matriz es simétrica, y en caso de
-        // encontrar un caso donde t[i][j] sea distinta de t[j][i] pondremos
-        // simétrica a falso.
-        // una solución es mirar todos los elementos de la matriz, pero se hacen comprobaciones
-        // dobles, un ejemplo: comprobamos t[1][2] con t[2][1]... pero más tarde comprobaremos
-        // t[2][1] con t[1][2]
-        // la solución será mirar solo la zona inferior o superior a la diagonal principal.
-        // En el momento que tengamos la constancia de que no es simétrica, pararemos
-        // todas las comprobaciones
-        i=0;
-        while(i<4 && simetrica==true){
-            j=0;
+        simetrica=true; // suponemos que la matriz es simétrica
+                        // el algoritmo de abajo se encargará de comprobar
+                        // si la matriz es simetrica en caso de no ser asi
+                        // cambiara el valor de simetrica a false
+        i = 0;
+        while(i<3 && simetrica==true){
+            j = 0;
             while(j<i && simetrica==true){
                 if(t[i][j]!=t[j][i])
                     simetrica=false;
@@ -43,9 +36,11 @@ public class MatrizSimetrica {
             }
             i++;
         }
-        // si en algún momento se da: t[i][j]!=t[j][i] es que la matriz no es simétrica.
-        // si al llegar aquí y la variable simétrica vale true, indica que no hemos encontrado
-        // ningún valor que indique que la matriz no es simétrica.
+        
+        // si en algún momento se da: t[i][j]!=t[j][i] es que la matriz 
+        //no es simétrica.
+        // si al llegar aquí y la variable simétrica vale true, indica que no 
+        // hemos encontrado ningún valor que indique que la matriz no es simétrica.
         if(simetrica)
             System.out.println("SIMETRICA");
         else
